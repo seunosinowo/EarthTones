cb08b29928d52d02d6b93689909e3f3222cea85a
seunosinowo <oluwaseunpaul98@gmail.com>
2025-03-14 09:57:03 +0100

    Implemented Mobile responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Menu, X } from 'lucide-react';
+import upIcon from '../assets/greenleaf.png';
+import downIcon from '../assets/widerleaf.png';
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,35 +16,35 @@ const NavBar = () => {
         <div className='text-white font-bold sm:text-2xl md:text-3xl lg:text-4xl'>
           EarthTones
         </div>
+
         <button className='text-white lg:hidden' onClick={toggleMenu}>
           {isOpen ? <X size={28} /> : <Menu size={28} />}
         </button>
+
         <ul className='hidden lg:flex lg:gap-8'>
-          <li><a href='#' className='nav-link text-white'>Home</a></li>
-          <li><a href='#' className='nav-link text-white'>About</a></li>
-          <li><a href='#' className='nav-link text-white'>Popular</a></li>
-          <li><a href='#' className='nav-link text-white'>Review</a></li>
+          <li><a href='#' className='nav-link text-white hover:text-yellow-500'>Home</a></li>
+          <li><a href='#' className='nav-link text-white hover:text-yellow-500'>About</a></li>
+          <li><a href='#' className='nav-link text-white hover:text-yellow-500'>Popular</a></li>
+          <li><a href='#' className='nav-link text-white hover:text-yellow-500'>Review</a></li>
         </ul>
       </div>
 
-      {/* <div className='absolute top-0 left-0 min-h-[80vh] w-full flex items-center justify-center duration-300'>
+      <div className='top-0 left-0 min-h-[80vh] w-full flex items-center justify-center duration-300'>
         <div className='absolute bottom-0 right-10 opacity-80'>
           <img src={upIcon} alt='leaf_image' className='w-36' />
         </div>
-        <div className='absolute top-0 left-5 rotate-90 opacity-80'>
+        <div className='absolute top-0 left-5 mt-20 rotate-90 opacity-80'>
           <img src={downIcon} alt='leaf_image' className='w-36' />
-        </div> */}
-
-
-      {/* </div> */}
+        </div>
+       </div>
 
       {isOpen && (
-        <div className='fixed left-0 top-16 w-full bg-green-200 p-12 flex flex-col justify-center items-center lg:hidden'>
-          <ul className='space-y-6'>
-            <li><a href='#' className='nav-link text-green-950'>Home</a></li>
-            <li><a href='#' className='nav-link text-green-950'>About</a></li>
-            <li><a href='#' className='nav-link text-green-950'>Popular</a></li>
-            <li><a href='#' className='nav-link text-green-950'>Review</a></li>
+        <div className='fixed left-0 top-16 w-full bg-slate-200 p-12 flex flex-col justify-center items-center lg:hidden'>
+          <ul className='space-y-6 text-green-950'>
+            <li><a href='#' className='nav-link hover:underline'>Home</a></li>
+            <li><a href='#' className='nav-link hover:underline'>About</a></li>
+            <li><a href='#' className='nav-link hover:underline'>Popular</a></li>
+            <li><a href='#' className='nav-link hover:underline'>Review</a></li>
           </ul>
         </div>
       )}
